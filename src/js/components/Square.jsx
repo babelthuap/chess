34fe8029e50a30bfc584dae6cd27eb23ee01e03a
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-02-12 17:01:56 -0800

    Prep for initial Heroku deployment

--- src/js/components/Square.jsx
+++ src/js/components/Square.jsx
@@ -5,10 +5,15 @@ class Square extends React.Component {
     super(props);
     this.displayName = 'Square';
   }
+
+  handleClick() {
+    console.log('position', this.props.coordinates)
+  }
+
   render() {
     // console.log(this.props.coordinates)
     return (
-      <div className={`${this.props.color} square`}>
+      <div className={`${this.props.color} square`} onClick={this.handleClick.bind(this)}>
         <span className="piece">{this.props.piece}</span>
       </div>
     );
